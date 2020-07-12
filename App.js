c16c69c623447ca5b6c7f8468a97eb6dc2a024e2
Ine Malen Stock <inemalenstock@Ines-MacBook-Pro.local>
2020-07-12 12:42:37 +0200

    endret på alignItems

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
@@ -15,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
   },
 });
